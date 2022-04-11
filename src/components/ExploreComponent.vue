e6d22cbdad6d3baf1998412adff51a49ded5ce36
alanna <alanna@sidedooraccess.com>
2022-04-10 23:12:04 -0300

    update

--- src/components/ExploreComponent.vue
+++ src/components/ExploreComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="explore">
     <div class="play" @click="toggleAudio">
-      <div :text="text">{text}</div>
+      <div :text="text">{{ text }}</div>
     </div>
 
     <div class="audio-item">
@@ -61,10 +61,6 @@
     methods: {
       toggleAudio() {
         const audio = this.$refs.audio;
-        // const audio = document.getElementById("body-audio");
-        console.log(audio.paused);
-
-        console.log(process.env.VUE_APP_BUCKETEER_BUCKET_NAME);
 
         if (audio.paused) {
           console.log('gets here');
