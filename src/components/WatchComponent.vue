82b5e9ad465d1774e7a9ab7825937709434edca7
alanna <alanna@sidedooraccess.com>
2022-04-16 04:38:55 -0300

    update video types

--- src/components/WatchComponent.vue
+++ src/components/WatchComponent.vue
@@ -1,11 +1,10 @@
 <template>
-  <video
-    :src="`https://${bucket}.s3.amazonaws.com/public/final.mp4`"
-    type='video/mp4'
-    controls
-  />
   <video id="sampleMovie" width="640" height="360" preload controls>
+    <source :src="`https://${bucket}.s3.amazonaws.com/public/final.m4v`" />
     <source :src="`https://${bucket}.s3.amazonaws.com/public/final.mp4`" />
+    <source :src="`https://${bucket}.s3.amazonaws.com/public/final.ogv`" />
+    <source :src="`https://${bucket}.s3.amazonaws.com/public/final.wemb`" />
+    Your browser does not support the video tag.
   </video>
 </template>
 
