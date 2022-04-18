0f8bb2a71388346ab3b9d1a1f3d1825fac17d95f
alanna <alanna@sidedooraccess.com>
2022-04-18 17:04:48 -0300

    update alt text

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -2,7 +2,7 @@
   <div class="paragraph">
     <img
       :src="`https://${bucket}.s3.amazonaws.com/public/still.jpeg`"
-      alt="Image of the artist and their child"
+      alt="Photo of parent and child viewed from behind standing on rocks at the ocean's edge. They are holding hands and looking off to the peach-coloured sunrise, with a circular apparition of seals in the distance."
       class="about-image"
     />
   </div>
